659de76c591952e315cee2ee1d747ddb9702e5f4
John Keaney <54217957+Keaneyjo@users.noreply.github.com>
2020-02-04 11:56:39 +0000

    Mostly working. Clean up needed to repeat values.

--- Practical One - Binary Lightup Board Display/App.s
+++ Practical One - Binary Lightup Board Display/App.s
@@ -1,14 +1,22 @@
-	AREA	AsmTemplate, CODE, READONLY
+
+
+	AREA	AsmTemplate, CODE, READWRITE
 	IMPORT	main
 	
 ; Written by John Keaney (Github: keaneyjo)
 
+ARR_R	SPACE	4 *	32	; 4 * 4 * word-size values
+
+		
+		
 	EXPORT	start
 start
 
 IO1DIR	EQU	0xE0028018
 IO1SET	EQU	0xE0028014
 IO1CLR	EQU	0xE002801C
+	
+VALUE	EQU 0x00000419
 	
 		LDR	R1,=IO1DIR
 		LDR	R2,=0x000f0000		;select P1.19--P1.16
@@ -20,156 +28,85 @@ IO1CLR	EQU	0xE002801C
 ; R2 points to the CLEAR register
 
 ;		LDR	R5,=0x00100000		; end when the mask reaches this value
-wloop	LDR	R3,=0x00010000		; start with P1.16.
-floop	STR	R3,[R2]	   			; clear the bit -> turn on the LED
+;wloop	LDR	R3,=0x00010000		; start with P1.16.
+;floop	STR	R3,[R2]	   			; clear the bit -> turn on the LED
 
 
 
-		STR	R3,[R1]			;set the bit -> turn off the LED
-		MOV	R3,R3,LSL #1	;shift up to next bit. P1.16 -> P1.17 etc.
-		CMP	R3,R5
-		BNE	floop
-		B	wloop
-stop	B	stop
-		; R3 is the LED
-		; Storing R3 in R1 turns LED off
-		; Storing R3 in R2	turns LED on
-		
-startup
-		LDR R5, =0x00000419		; Place hex value here, sample given =0x00000419 or in decimal 1049 
-		LDR R11, [R5]			; clone of initial value
-		
-		MOV R7, #0  ; 4 bit count
-		MOV R8, #0	; single bit count
-		B	truemain
-		;LDR R0, =0x01 ; R0 = 0x00FF (255)
-		;MOV R1, R0, LSL #1 ; R1 = 0x01FE (510)
-		
-newnibble
-		ADD R7, R7, #1
-		MOV R8, #0
-		B	endnewnibble
-		
-		;skip ahead of leading zeros by counting them for later
-		;count of leading zeros stored in R8
-leadingzeros
-		MOV R11, R11, LSL #1 ; R1 = 0x01FE (510)
-		AND R10, R11, R6
-		CMP R10, #1
-		BEQ	endleadingzeros
-		ADD R8, R8, #1			; now I know how many leading zeros
-		CMP R8, #3
-		BEQ newnibble
-endnewnibble
-		B	leadingzeros
+;		STR	R3,[R1]			;set the bit -> turn off the LED
+;		MOV	R3,R3,LSL #1	;shift up to next bit. P1.16 -> P1.17 etc.
+;		CMP	R3,R5
+;		BNE	floop
+;		B	wloop
+
+; R3 is the LED
+; Storing R3 in R1 turns LED off
+; Storing R3 in R2	turns LED on
 
 
-truemain
-		B	leadingzeros
-endleadingzeros
-		LDR R12, [R8]		; recreate 32 bit count
-whileloop
-		LDR R11, [R5]		; recreate clone
-		LDR R6, =0x80000000	;mask
-		AND R9, R11, R6		;MSB = or =/= 1, i.e. number is negative
-		CMP R9, #1
-		BEQ	negative
-returnnnegative
-; delay used in function
 
-		MOV	R4, #4
-		MUL R10, R8, R4
-		MOV R8, #1
-		LDR R6, =0x80000000
 
-removeleadingzeros
-		CMP R8, R10
-		BEQ	spareclone
-		;LDR R0, =0x01 ; R0 = 0x00FF (255)
-		;MOV R1, R0, LSL #1 ; R1 = 0x01FE (510)
-		MOV R11, R11, LSL #1
-		ADD R8, R8, #1
-		B	removeleadingzeros
+startup 
+		LDR R5, =VALUE		; Place hex value here, sample given =0x00000419 or in decimal 1049 	;n
+		LDR	R6, =table		; mod address
+		LDR R7, =0x0		; count
+		LDR R8, [R6]
+		LDR R10, =0x40000000
+		LDR R9, =0x0
+		LDR R12, =0x0
+
+while1
+		CMP R5, #10
+		BLT endwhile1
+;		{
+while2
+		CMP R5, R8
+		BLT endwhile2
+;				{
+		ADD R7, R7, #1	; count++
+		SUB R5, R5, R8	; n = n - mod
+;				}
+		B	while2
+endwhile2
+		;MOV R11, R11, LSL #1
+		LDR R8, [R6, #4]!				;mod = mod LSL #2
 		
-spareclone
-		LDR R9, [R11]
+		CMP R7, #0
+		BEQ	skip1						;if(count != 0)
+		LDR R9, =0x01					;	leadingzeros = 1
+skip1		
 		
-lightup
-		CMP	R12, #31
-		BEQ	endlightup
+		CMP R9, #0
+		BEQ skip2
+		STR R7, [R10], #4				; store count
+		ADD R12, R12, #1
+skip2
+		LDR R7, =0x0					; count = 0
 		
-; first check if quadruple zeros
-		MOV R9, #0
-;first bit
-		AND R10, R11, R6
-		CMP R10, #0
-		BNE skip
-		ADD R9, R9, #1
-;second bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		CMP R10, #0
-		BNE skip
-		ADD R9, R9, #1
-;third bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		CMP R10, #0
-		BNE skip
-		ADD R9, R9, #1
-;fourth bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		CMP R10, #0
-		B	zeropresent
-returnzeropresent
-		
-		
-		
-skip
-		LDR R11, [R9]
-		LDR	R3,=0x00010000		; start with P1.16.
-		
-;first bit
-		AND R10, R11, R6
-		CMP R10, #0
-		BEQ lightoff
-		BNE	lighton
-;second bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		BEQ lightoff
-		BNE	lighton
-;third bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		CMP R10, #0
-		BEQ lightoff
-		BNE	lighton
-;fourth bit
-		MOV R11, R11, LSL #1
-		AND R10, R11, R6
-		CMP R10, #0
-		BEQ lightoff
-		BNE	lighton
-		
-		LDR R9, [R11]
-		B	lightup
-		
-endlightup
-		B whileloop
+		B while1
+endwhile1
+		STR R5, [R10]					; store count
+
+	; your program goes here
 	
-		
-		
-lighton
-		STR	R3,[R2]	   			; clear the bit -> turn on the LED
-		BX	LR
-		
-lightoff
-		STR	R3,[R1]				;set the bit -> turn off the LED
-		BX	LR
-			
-			
+;division (+remove leading zeros)
+;while(true)
+;{
+;	restart value
+;	check negative (+delay)
+;	for(int i = 0; i < R12; i++)
+;	{
+;		if(0) {1,1,1,1}
+;		else {lightOn}
+;		shiftToNextValue
+;	}
+;}
+	
+whileloop1	
+; Negative
+		LDR R5, =VALUE		; Restart value
+		CMP R5, #0
+		BGE	notNegative
 negative
 		LDR	R3,=0x00010000		; start with P1.16.
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
@@ -179,17 +116,21 @@ negative
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
 		MOV	R3,R3,LSL #1		; shift up to next bit. P1.18 -> P1.19 etc.
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
-		BL 	delay
-		B 	returnnnegative
-		
-delay
-		;delay for about a half second
 		LDR	R4,=40000000
-dloop	SUBS	R4,R4,#1
-		BNE	dloop
-		BX 	LR
-		; maybe use the BX 
-		
+dloop10	SUBS	R4,R4,#1
+		BNE	dloop10
+notNegative
+
+
+		LDR R7, =0x40000000		;
+		MOV R9, #0				;quadruple zeros count
+		LDR R8, [R7], #4
+
+whileloop2
+		CMP R12, R11
+		BEQ	endwhileloop2
+		CMP R8, #0			;if(0) {1,1,1,1}
+		BNE	noZero
 zeropresent
 		LDR	R3,=0x00010000		; start with P1.16.
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
@@ -199,7 +140,97 @@ zeropresent
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
 		MOV	R3,R3,LSL #1		; shift up to next bit. P1.18 -> P1.19 etc.
 		STR	R3,[R2]	   			; clear the bit -> turn on the LED
-		BL 	delay
-		B returnzeropresent
+		LDR	R4,=40000000
+dloop11	SUBS	R4,R4,#1
+		BNE	dloop11
+		
+noZero
+		
+		
+		
+		
+		
+		;R3 = light
+		;R4 = loop delay
+		;R5 = VALUE ? could be changed
+		;R6 = mask
+		; 
+		
+		;else {lightOn}
+		
+		LDR	R3,=0x00010000		; start with P1.16.
+		LDR R6,=0x00000008
+;first bit
+		AND R10, R8, R6
+		CMP R10, #0
+		BEQ lightoff1
+		STR	R3,[R2]	   			; clear the bit -> turn on the LED
+		B	skiplight1
+lightoff1
+		STR	R3,[R1]				; set the bit -> turn off the LED
+skiplight1
+
+
+;second bit
+		MOV	R3,	R3,	LSL #1		;shift up to next bit. P1.16 -> P1.17 etc.
+		MOV R6, R6, LSR #1
+		AND R10, R8, R6
+		CMP R10, #0
+		BEQ lightoff2
+		STR	R3,[R2]	   			; clear the bit -> turn on the LED
+		B	skiplight2
+lightoff2
+		STR	R3,[R1]				; set the bit -> turn off the LED
+skiplight2
+
+
+;third bit
+		MOV	R3, R3,	LSL #1	;shift up to next bit. P1.16 -> P1.17 etc.
+		MOV R6, R6, LSR #1
+		AND R10, R8, R6
+		CMP R10, #0
+		BEQ lightoff3
+		STR	R3,[R2]	   			; clear the bit -> turn on the LED
+		B	skiplight3
+lightoff3
+		STR	R3,[R1]				; set the bit -> turn off the LED
+skiplight3
+
+
+;fourth bit
+		MOV	R3, R3,	LSL #1	;shift up to next bit. P1.16 -> P1.17 etc.
+		MOV R6, R6, LSR #1
+		AND R10, R8, R6
+		CMP R10, #0
+		BEQ lightoff4
+		STR	R3,[R2]	   			; clear the bit -> turn on the LED
+		B	skiplight4
+lightoff4
+		STR	R3,[R1]				; set the bit -> turn off the LED
+skiplight4
+		
+		
+		
+		LDR R8, [R7], #4
+		ADD R11, R11, #1		
+
+;delayloop
+		LDR	R4,=40000000
+dloop2	SUBS	R4,R4,#1
+		BNE	dloop2
+		
+		
+		
+		B	whileloop2
+
+endwhileloop2
+		B	whileloop1
+		
+
+	
+STOP	B	STOP
+
+table	
+	DCD 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10
 		
 		END
